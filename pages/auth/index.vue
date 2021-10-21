80db5f932945cf26dce055f4afd64bfa02b92ae6
Patrick <patrick.kosir@gmail.com>
2021-10-21 10:23:47 +0200

    Debug settings for auth

--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -6,8 +6,14 @@
 import jwtDecode from 'jwt-decode'
 export default {
   created() {
+    try {
     const decodedBearer = jwtDecode(this.$auth.$storage.getUniversal('_token.aad'))
+    console.log(decodedBearer)
     this.$auth.$storage.setUniversal('jwt_decoded', decodedBearer)
+    }
+    catch (error) {
+    console.error(error);
+    }
   },
 }
 </script>
