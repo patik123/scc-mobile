184149f347e6d6126a44aa88ba67cc3696adf328
Patrick <69119220+patik123@users.noreply.github.com>
2022-01-07 10:48:40 +0100

    Onemogočitev desnega klika

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="noselect">
+  <div class="noselect" @contextmenu="prevent_right_click($event)">
     <Nuxt />
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    prevent_right_click(e) {
+        e.preventDefault();
+    }
+  }
+}
+</script>
